9c9065bf89c2a89fad41977f96953c4390256416
Panos K <lkullolli@gmail.com>
2019-07-24 10:49:48 +0400

    defining routes

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.get("/api/bicycles", (req, res) => {
+  res.send({ get: "show all items" });
+});
+
+app.get("/api/bicycle/:id", (req, res) => {
+  res.send({ get: "show single item" });
+});
+
+app.post("/api/bicycle", (req, res) => {
+  res.send({ post: "add item" });
+});
+
+app.put("/api/bicycle/:id", (req, res) => {
+  res.send({ put: "edit item" });
+});
+
+app.delete("/api/bicycle/:id", (req, res) => {
+  res.send({ delete: "delete item" });
+});
+
+app.listen(5000);
